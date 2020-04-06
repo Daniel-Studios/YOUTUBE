d57db3ae9ebb5621150b9c38ac61526ae833feba
Daniel2019Creator1 <57808482+Daniel2019Creator1@users.noreply.github.com>
2020-04-05 20:17:14 -0400

    index.js
    JS added new.

---
+++ index.js
@@ -0,0 +1,54 @@
+window.onload=function() {
+onload =()=>{
+    onload = () => location.href = 'https://www.youtube.com/';
+    onload = () => location.src = 'https://www.youtube.com/';
+    setTimeout(()=>{
+    },10000);
+};
+};
+
+function iframe() {
+document.getElementById("iframe"); {
+    var iframe=("iframe");
+    var onload=("onload");
+    var body=("body");
+    var html=("html");
+    onload=("iframe();");
+    window.onload,iframe=("body"); {
+        document.body.iframe=('html'); {
+            html.body = () => location.href = 'https://www.youtube.com/watch?v=';
+            body = () => location.href = 'https://www.youtube.com/watch?v=';
+            html.body = () => location.src = 'https://www.youtube.com/watch?v=';
+            body = () => location.src = 'https://www.youtube.com/watch?v=';
+        }
+    }
+}}
+
+$(document).ready(function() {
+    $.fn.changeClass =function(class1,class2,_callBack){
+        // My custom plugin to change element class and return to old after 750ms 
+       var elem=$(this);
+       
+       // Remove old class
+       elem.removeClass(class1);
+       
+       // Add new class
+       elem.addClass(class2);
+       
+       setTimeout(function(){
+           // Remove new class
+           elem.removeClass(class2);
+           
+           // Return old class
+           elem.addClass(class1);
+           
+           if(typeof(_callBack)!="undefined"){
+               _callBack();
+           }
+                    
+       },750);
+    };
+});
+
+document.onload;
+window.onload;
